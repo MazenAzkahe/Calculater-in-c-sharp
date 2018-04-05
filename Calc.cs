e86d2e7b8ae2524bf72ee9779f77092223d6ed7f
Mazen AZKAHE <ENGMAZENN93@Outlook.com>
2018-04-04 22:25:18 -0700

    Calculater Class
    Simple Class tryying in cSharp

---
+++ Calc.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Class
+{
+    class Calc
+    {
+        double _Number1;
+        double _Number2;
+        public Calc(double Number1,double Number2)
+        {
+            this._Number1 = Number1;
+            this._Number2 = Number2;
+        }
+
+
+        private double Add()
+        {
+            return _Number1 + _Number2;
+        }
+
+        public void PrintAdd()
+        {
+            Console.WriteLine(Add());
+        }
+
+        private double Sub()
+        {
+            return _Number1 - _Number2;
+        }
+
+        public void PrintSub()
+        {
+            Console.WriteLine(Sub());
+        }
+        private double Div()
+        {
+            return _Number1 * _Number2;
+        }
+        public void PrintDiv()
+        {
+            Console.WriteLine(Div());
+        }
+
+        private double Multi()
+        {
+            if(_Number2==0)
+            {
+                _Number2 = 1;
+            }
+            return _Number1 / _Number2;
+        }
+
+        public void PrintMulti()
+        {
+            Console.WriteLine(Multi());
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calc = new Calc(5, 6);
+            calc.PrintAdd();
+            calc.PrintSub();
+            calc.PrintDiv();
+            calc.PrintMulti();
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
